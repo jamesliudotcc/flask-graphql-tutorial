67e8c2cd74a6847426d22275aca2bd31b6f4ca69
James Liu <james@jamesliu.cc>
2019-11-15 18:09:06 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+From this tutorial: https://medium.com/@marvinkome/creating-a-graphql-server-with-flask-ae767c7e2525
+
+Finally, it appears the python environment works.
+
+Activate using `pipenv shell`, leave using `exit`
+
+at Adding GraphQL
---
+++ app.py
@@ -0,0 +1,54 @@
+# Imports
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.debug = True
+
+# Configs
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+# Modules
+db = SQLAlchemy(app)
+
+# Models
+class User(db.Model):
+    __tablename__ = 'users'
+
+    uuid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(256), index=True, unique=True)
+    posts = db.relationship('Post', backref='author')
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    uuid = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256), index=True)
+    body = db.Column(db.Text)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.uuid'))
+
+    def __repr__(self):
+        return f"<Post {self.title}>"
+
+# Schema Objects
+# TODO
+
+# Routes
+# TODO
+
+
+@app.route('/')
+def index():
+    return '<p>Hello World</p>'
+
+if __name__ == '__main__':
+    app.run()
